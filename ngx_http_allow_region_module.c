f26dc6a84cdcb5a52a3224f0276c550773306b45
m-yamamoto <m-yamamoto@sakura.ad.jp>
2017-11-01 10:22:59 +0900

    region_checkをifディレクティブでも使えるように

--- ngx_http_allow_region_module.c
+++ ngx_http_allow_region_module.c
@@ -58,7 +58,7 @@ static ngx_command_t  ngx_http_allow_region_commands[] = {
       NULL },
 
     { ngx_string("region_check"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_FLAG,
       ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_allow_region_loc_conf_t, enable),
